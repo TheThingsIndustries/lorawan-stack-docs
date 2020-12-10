ef392115d045efd23ad5fb58e60d44f9375a2269
michalborkowski96 <michalborkowski96@yahoo.pl>
2020-12-10 14:02:55 +0000

    Deploying to gh-pages from  @ 451cec39eee18705328b35286e9889467dd51fbc 🚀

--- js/theme.min.e9df3c06e815b9686686212e86bfd0217a508f5ccee0139d7bc899ef7460191e.js
+++ js/theme.min.e9df3c06e815b9686686212e86bfd0217a508f5ccee0139d7bc899ef7460191e.js
@@ -1,4 +1,9 @@
-document.addEventListener('DOMContentLoaded',function(){var $navbarBurgers=Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'),0)
+document.addEventListener('DOMContentLoaded',function(){var migrationInfoAccepted=window.localStorage.getItem('migrationInfoAccepted')
+if(migrationInfoAccepted!='true'){var migrationBar=document.getElementById('migration-info')
+migrationBar.style.display='flex';var migrationButton=document.getElementById('migration-button')
+migrationButton.addEventListener('click',function(){window.localStorage.setItem('migrationInfoAccepted',true)
+migrationBar.style.display='none';})}
+var $navbarBurgers=Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'),0)
 if($navbarBurgers.length>0){$navbarBurgers.forEach(function(el){el.addEventListener('click',function(){var target=el.dataset.target
 var $target=document.getElementById(target)
 el.classList.toggle('is-active')
