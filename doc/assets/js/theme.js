9e0d6dc7dd9b3ce4d10a1b826659f3e58a73e13f
Ben Olayinka <ben.olayinka@gmail.com>
2021-06-18 10:44:44 +0200

    util: Activate tabs with anchor links (#381)
    * util: Activate tabs with anchor links
    
    * fixup! util: Activate tabs with anchor links

--- doc/assets/js/theme.js
+++ doc/assets/js/theme.js
@@ -26,65 +26,76 @@ function addNavBarBurgers(){
   }
 }
 
-function addAnchor(element) {
-  element.insertAdjacentHTML('beforeend',
-  `<a class="header-hash" href="#${element.id}" ariaLabel="Anchor">#</a>` )
-}
+function initializeTabsAndAnchors () {
+  var anchorTable = {}
 
-function addAnchors() {
-  var headers = document.querySelectorAll(
-    '.docs-content h2[id], .docs-content h3[id], .docs-content h4[id], .docs-content h5[id], .docs-content h6[id]')
-    if (headers) {
-        headers.forEach(addAnchor)
+  function createAnchorTable(element) {
+    const tabContainer = element.closest('.tab-content')
+    if(tabContainer) {
+      const tab = element.parentElement
+      const tabName = tab.getAttribute('data-content')
+      anchorTable[element.id] = tabName
     }
-}
-
-function addTabs(){
-  const tabs = [...document.querySelectorAll('.tabs li')]
-  const tabContent = [...document.querySelectorAll('.tab-content section')]
-  const activeClass = 'is-active'
-
-  function init() {
-    if (window.sessionStorage.getItem('tabActive')) {
-      toggleTab(window.sessionStorage.getItem('tabActive'))
-    } else if (tabs && tabs[0]) {
-      let key = tabs[0].getAttribute('data-tab')
-      toggleTab(key)
-    }
-
-    tabs.forEach(tab => {
-      tab.addEventListener('click', handleClick)
-    })
-
-    document.querySelectorAll('figure').forEach(fig => {
-      fig.addEventListener('click', event => {
-        basicLightbox.create(`<img src="${event.target.src}" alt="${event.target.alt}" />`).show()
-      })
-    })
   }
 
-  function handleClick(event) {
-    event.preventDefault()
-    let tab = event.currentTarget
-    let key = tab.getAttribute('data-tab')
+  function addAnchor(element) {
+    element.insertAdjacentHTML('beforeend',
+    `<a class="header-hash" href="#${element.id}" ariaLabel="Anchor">#</a>` )
+  }
+
+  function addAnchors() {
+    var headers = document.querySelectorAll(
+      '.docs-content h2[id], .docs-content h3[id], .docs-content h4[id], .docs-content h5[id], .docs-content h6[id]')
+      if (headers) {
+          headers.forEach(addAnchor)
+          headers.forEach(createAnchorTable)
+      }
+  }
+
+  function hashChanged() {
+    const hash = window.location.hash
+    const id = window.location.hash.substring(1)
+    const key = anchorTable[id]
     toggleTab(key)
+    document.getElementById(id).scrollIntoView()
+  }
+
+  function addOnHashChange () {
+    if ('onhashchange' in window) { // event supported?
+      window.onhashchange = function () {
+        hashChanged(window.location.hash);
+      }
+    }
+    else { // event not supported:
+        var storedHash = window.location.hash;
+        window.setInterval(function () {
+            if (window.location.hash != storedHash) {
+                storedHash = window.location.hash;
+                hashChanged(storedHash);
+            }
+        }, 1000);
+    }
   }
 
   function toggleTab(key) {
+    const activeClass = 'is-active'
+
+    function getTabsByKey(key) {
+      return [...document.querySelectorAll(`[data-tab="${key}"]`)]
+    }
+
     let activeTabs = getTabsByKey(key)
 
-    if (!activeTabs) return
+    if (activeTabs.length === 0) return false
 
     tabs.forEach(tab => {
       if (tab && tab.classList.contains(activeClass)) {
         tab.classList.remove(activeClass)
       }
     })
-
     activeTabs.forEach(tab => {
       tab.classList.add(activeClass)
     })
-
     tabContent.forEach(item => {
       if (item && item.classList.contains(activeClass)) {
         item.classList.remove(activeClass)
@@ -97,17 +108,57 @@ function addTabs(){
     if (window.sessionStorage) {
       window.sessionStorage.setItem('tabActive', key)
     }
+
+    return true
   }
 
-  function getTabsByKey(key) {
-    return [...document.querySelectorAll(`[data-tab="${key}"]`)]
+  const tabs = document.querySelectorAll('.tabs li')
+  const tabContent = document.querySelectorAll('.tab-content section')
+
+  function addTabs(){
+
+    if(tabs.length===0) return
+
+    function handleClick(event) {
+      event.preventDefault()
+      let tab = event.currentTarget
+      let key = tab.getAttribute('data-tab')
+      toggleTab(key)
+    }
+
+    if (window.sessionStorage.getItem('tabActive')) {
+      const success = toggleTab(window.sessionStorage.getItem('tabActive'))
+      if (success === false) {
+        let key = tabs[0].getAttribute('data-tab')
+        toggleTab(key)
+      }
+    } else if (tabs && tabs[0]) {
+      let key = tabs[0].getAttribute('data-tab')
+      toggleTab(key)
+    }
+
+    tabs.forEach(tab => {
+      tab.addEventListener('click', handleClick)
+    })
+
   }
 
-  init()
+  addAnchors()
+  addTabs()
+  addOnHashChange()
+
+}
+
+function addLightboxHandler() {
+  document.querySelectorAll('figure').forEach(fig => {
+    fig.addEventListener('click', event => {
+      basicLightbox.create(`<img src="${event.target.src}" alt="${event.target.alt}" />`).show()
+    })
+  })
 }
 
 document.addEventListener('DOMContentLoaded', function () {
-  addAnchors()
   addNavBarBurgers()
-  addTabs()
+  initializeTabsAndAnchors()
+  addLightboxHandler()
 })
