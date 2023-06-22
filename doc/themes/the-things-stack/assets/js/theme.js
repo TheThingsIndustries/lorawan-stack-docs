c88e1ef601b42cd82264d7b08778356741ec78a7
Krishna Iyer Easwaran <krishna@thethingsindustries.com>
2023-06-22 10:39:42 +0200

    doc: Revert formatting

--- doc/themes/the-things-stack/assets/js/theme.js
+++ doc/themes/the-things-stack/assets/js/theme.js
@@ -16,151 +16,153 @@ import "@ttui/design/ui/ttui-vendor.js";
 import "@ttui/design/ui/main.js";
 import "@ttui/design/ui/components/heading-expanded/index.js";
 
-function addNavBarBurgers() {
-  var $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll(".navbar-burger"), 0);
+function addNavBarBurgers(){
+  var $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0)
   if ($navbarBurgers.length > 0) {
-    $navbarBurgers.forEach(function (el) {
-      el.addEventListener("click", function () {
-        var target = el.dataset.target;
-        var $target = document.getElementById(target);
-        el.classList.toggle("is-active");
-        $target.classList.toggle("is-active");
-      });
-    });
+    $navbarBurgers.forEach(function(el) {
+      el.addEventListener('click', function() {
+        var target = el.dataset.target
+        var $target = document.getElementById(target)
+        el.classList.toggle('is-active')
+        $target.classList.toggle('is-active')
+      })
+    })
   }
 }
 
-function initializeTabsAndAnchors() {
-  var anchorTable = {};
+function initializeTabsAndAnchors () {
+  var anchorTable = {}
 
   function createAnchorTable(element) {
-    const tabContainer = element.closest(".tab-content");
-    if (tabContainer) {
-      const tab = element.parentElement;
-      const tabName = tab.getAttribute("data-content");
-      anchorTable[element.id] = tabName;
+    const tabContainer = element.closest('.tab-content')
+    if(tabContainer) {
+      const tab = element.parentElement
+      const tabName = tab.getAttribute('data-content')
+      anchorTable[element.id] = tabName
     }
   }
 
   function addAnchor(element) {
-    element.insertAdjacentHTML("beforeend", `<a class="header-hash" href="#${element.id}" ariaLabel="Anchor">#</a>`);
+    element.insertAdjacentHTML('beforeend',
+    `<a class="header-hash" href="#${element.id}" ariaLabel="Anchor">#</a>` )
   }
 
   function addAnchors() {
     var headers = document.querySelectorAll(
-      ".docs-content h2[id], .docs-content h3[id], .docs-content h4[id], .docs-content h5[id], .docs-content h6[id]"
-    );
-    if (headers) {
-      headers.forEach(addAnchor);
-      headers.forEach(createAnchorTable);
-    }
+      '.docs-content h2[id], .docs-content h3[id], .docs-content h4[id], .docs-content h5[id], .docs-content h6[id]')
+      if (headers) {
+          headers.forEach(addAnchor)
+          headers.forEach(createAnchorTable)
+      }
   }
 
   function hashChanged() {
-    const hash = window.location.hash;
-    const id = window.location.hash.substring(1);
-    const key = anchorTable[id];
-    toggleTab(key);
-    document.getElementById(id).scrollIntoView();
+    const hash = window.location.hash
+    const id = window.location.hash.substring(1)
+    const key = anchorTable[id]
+    toggleTab(key)
+    document.getElementById(id).scrollIntoView()
   }
 
-  function addOnHashChange() {
-    if ("onhashchange" in window) {
-      // event supported?
+  function addOnHashChange () {
+    if ('onhashchange' in window) { // event supported?
       window.onhashchange = function () {
         hashChanged(window.location.hash);
-      };
-    } else {
-      // event not supported:
-      var storedHash = window.location.hash;
-      window.setInterval(function () {
-        if (window.location.hash != storedHash) {
-          storedHash = window.location.hash;
-          hashChanged(storedHash);
-        }
-      }, 1000);
+      }
+    }
+    else { // event not supported:
+        var storedHash = window.location.hash;
+        window.setInterval(function () {
+            if (window.location.hash != storedHash) {
+                storedHash = window.location.hash;
+                hashChanged(storedHash);
+            }
+        }, 1000);
     }
   }
 
   function toggleTab(key) {
-    const activeClass = "is-active";
+    const activeClass = 'is-active'
 
     function getTabsByKey(key) {
-      return [...document.querySelectorAll(`[data-tab="${key}"]`)];
+      return [...document.querySelectorAll(`[data-tab="${key}"]`)]
     }
 
-    let activeTabs = getTabsByKey(key);
+    let activeTabs = getTabsByKey(key)
 
-    if (activeTabs.length === 0) return false;
+    if (activeTabs.length === 0) return false
 
-    tabs.forEach((tab) => {
+    tabs.forEach(tab => {
       if (tab && tab.classList.contains(activeClass)) {
-        tab.classList.remove(activeClass);
+        tab.classList.remove(activeClass)
       }
-    });
-    activeTabs.forEach((tab) => {
-      tab.classList.add(activeClass);
-    });
-    tabContent.forEach((item) => {
+    })
+    activeTabs.forEach(tab => {
+      tab.classList.add(activeClass)
+    })
+    tabContent.forEach(item => {
       if (item && item.classList.contains(activeClass)) {
-        item.classList.remove(activeClass);
+        item.classList.remove(activeClass)
       }
-      let data = item.getAttribute("data-content");
+      let data = item.getAttribute('data-content')
       if (data === key) {
-        item.classList.add(activeClass);
+        item.classList.add(activeClass)
       }
-    });
+    })
     if (window.sessionStorage) {
-      window.sessionStorage.setItem("tabActive", key);
+      window.sessionStorage.setItem('tabActive', key)
     }
 
-    return true;
+    return true
   }
 
-  const tabs = document.querySelectorAll(".tabs li");
-  const tabContent = document.querySelectorAll(".tab-content section");
+  const tabs = document.querySelectorAll('.tabs li')
+  const tabContent = document.querySelectorAll('.tab-content section')
 
-  function addTabs() {
-    if (tabs.length === 0) return;
+  function addTabs(){
+
+    if(tabs.length===0) return
 
     function handleClick(event) {
-      event.preventDefault();
-      let tab = event.currentTarget;
-      let key = tab.getAttribute("data-tab");
-      toggleTab(key);
+      event.preventDefault()
+      let tab = event.currentTarget
+      let key = tab.getAttribute('data-tab')
+      toggleTab(key)
     }
 
-    if (window.sessionStorage.getItem("tabActive")) {
-      const success = toggleTab(window.sessionStorage.getItem("tabActive"));
+    if (window.sessionStorage.getItem('tabActive')) {
+      const success = toggleTab(window.sessionStorage.getItem('tabActive'))
       if (success === false) {
-        let key = tabs[0].getAttribute("data-tab");
-        toggleTab(key);
+        let key = tabs[0].getAttribute('data-tab')
+        toggleTab(key)
       }
     } else if (tabs && tabs[0]) {
-      let key = tabs[0].getAttribute("data-tab");
-      toggleTab(key);
+      let key = tabs[0].getAttribute('data-tab')
+      toggleTab(key)
     }
 
-    tabs.forEach((tab) => {
-      tab.addEventListener("click", handleClick);
-    });
+    tabs.forEach(tab => {
+      tab.addEventListener('click', handleClick)
+    })
+
   }
 
-  addAnchors();
-  addTabs();
-  addOnHashChange();
+  addAnchors()
+  addTabs()
+  addOnHashChange()
+
 }
 
 function addLightboxHandler() {
-  document.querySelectorAll("figure").forEach((fig) => {
-    fig.addEventListener("click", (event) => {
-      basicLightbox.create(`<img src="${event.target.src}" alt="${event.target.alt}" />`).show();
-    });
-  });
+  document.querySelectorAll('figure').forEach(fig => {
+    fig.addEventListener('click', event => {
+      basicLightbox.create(`<img src="${event.target.src}" alt="${event.target.alt}" />`).show()
+    })
+  })
 }
 
-document.addEventListener("DOMContentLoaded", function () {
-  addNavBarBurgers();
-  initializeTabsAndAnchors();
-  addLightboxHandler();
-});
+document.addEventListener('DOMContentLoaded', function () {
+  addNavBarBurgers()
+  initializeTabsAndAnchors()
+  addLightboxHandler()
+})
